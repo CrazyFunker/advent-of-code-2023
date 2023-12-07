c202fa62a18086483b5aff79369741f72d8838e7
Mateusz Swirski <mateusz@payzilch.com>
2023-12-07 17:51:24 +0000

    solved day 7

--- advent_of_code_2023_7/src/main.rs
+++ advent_of_code_2023_7/src/main.rs
@@ -9,7 +9,6 @@ struct Hand {
     cards: String,
     bid: u32,
     rank: u32,
-    jokers: u32,
     joker_rank: u32,
 }
 
@@ -17,7 +16,6 @@ fn main() {
     let mut hands: Vec<Hand> = Vec::new();
     let mut total_winnings: u32 = 0;
     let mut total_winnings_v2: u32 = 0;
-    // read all hands and bids from file
 
     // read the input file
     let path: &Path = Path::new("input.txt");
@@ -39,14 +37,12 @@ fn main() {
                 let rank = get_card_rank(&cards_count);
 
                 // calculate jokers by looking up cards count for 'J'
-                let jokers = cards_count.get(&'J').unwrap_or(&0);
-                let joker_rank = get_card_rank_v2(&cards_count, jokers.to_owned());
+                let joker_rank = get_card_rank_v2(&cards_count);
 
                 let hand = Hand {
                     cards,
                     bid: parts[1].parse::<u32>().unwrap(),
                     rank,
-                    jokers: *jokers,
                     joker_rank: joker_rank,
                 };
                 println!("{:?}", hand);
@@ -81,12 +77,12 @@ fn main() {
     println!("Total winnings v2: {}", total_winnings_v2);
 }
 
-fn compare_hands_v2(hand1: &Hand, hand2: &Hand) -> Ordering {
-    if hand1.joker_rank > hand2.joker_rank {
+fn compare_hands(hand1: &Hand, hand2: &Hand) -> Ordering {
+    if hand1.rank > hand2.rank {
         return Ordering::Greater;
     }
 
-    if hand1.joker_rank < hand2.joker_rank {
+    if hand1.rank < hand2.rank {
         return Ordering::Less;
     }
 
@@ -106,19 +102,19 @@ fn compare_hands_v2(hand1: &Hand, hand2: &Hand) -> Ordering {
     return Ordering::Equal;
 }
 
-fn compare_hands(hand1: &Hand, hand2: &Hand) -> Ordering {
-    if hand1.rank > hand2.rank {
+fn compare_hands_v2(hand1: &Hand, hand2: &Hand) -> Ordering {
+    if hand1.joker_rank > hand2.joker_rank {
         return Ordering::Greater;
     }
 
-    if hand1.rank < hand2.rank {
+    if hand1.joker_rank < hand2.joker_rank {
         return Ordering::Less;
     }
 
     // if ranks are equal, compare cards from left to right
     for i in 0..5 {
-        let dif = card_char_to_number(hand1.cards.chars().nth(i).unwrap())
-            - card_char_to_number(hand2.cards.chars().nth(i).unwrap());
+        let dif = card_char_to_number_v2(hand1.cards.chars().nth(i).unwrap())
+            - card_char_to_number_v2(hand2.cards.chars().nth(i).unwrap());
 
         if dif > 0 {
             return Ordering::Greater;
@@ -207,7 +203,7 @@ fn get_card_rank(cards: &HashMap<char, u32>) -> u32 {
     return 0;
 }
 
-fn get_card_rank_v2(cards: &HashMap<char, u32>, jokers: u32) -> u32 {
+fn get_card_rank_v2(cards: &HashMap<char, u32>) -> u32 {
     // 7 Five of a kind, where all five cards have the same label: AAAAA
     // 6 Four of a kind, where four cards have the same label and one card has a different label: AA8AA
     // 5 Full house, where three cards have the same label, and the remaining two cards share a different label: 23332
@@ -215,6 +211,8 @@ fn get_card_rank_v2(cards: &HashMap<char, u32>, jokers: u32) -> u32 {
     // 3 Two pair, where two cards share one label, two other cards share a second label, and the remaining card has a third label: 23432
     // 2 One pair, where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4
     // 1 High card, where all cards' labels are distinct: 23456
+    let jokers = cards.get(&'J').unwrap_or(&0).to_owned();
+    println!("jokers: {}", jokers);
 
     if cards.keys().len() == 1 {
         // five of a kind
@@ -222,8 +220,10 @@ fn get_card_rank_v2(cards: &HashMap<char, u32>, jokers: u32) -> u32 {
     }
 
     // 22233
-    // 22223
     // 222JJ -> 7 Five of a kind
+    // JJJ33 -> 7 Five of a kind
+    // 22223
+    // JJJJ3 -> 7 Five of a kind
     // 2222J -> 7 Five of a kind
     if cards.keys().len() == 2 {
         if jokers > 0 {
@@ -244,34 +244,42 @@ fn get_card_rank_v2(cards: &HashMap<char, u32>, jokers: u32) -> u32 {
     }
 
     // 22234
+    // JJJ34 -> 6 four of kind
     // 22334
     // 2223J -> 6 four of kind
+    // 2233J -> 5 full house
     // 22JJ4 -> 6 four of kind
     if cards.keys().len() == 3 {
-        if jokers > 0 {
-            // four of a kind
-            return 6;
-        }
-
         // three of a kind or two pair
         for (_, count) in cards.iter() {
             if *count == 3 {
+                if jokers > 0 {
+                    // four of a kind
+                    return 6;
+                }
+
                 return 4;
             }
         }
+
+        if jokers == 1 {
+            // full house
+            return 5;
+        }
+
+        if jokers == 2 {
+            // four of kind
+            return 6;
+        }
+
         return 3;
     }
 
     // 23455
-    // J3455 -> 3 two pair
+    // J3455 -> 4 three of a kind
     // 234JJ -> 4 three of a kind
     if cards.keys().len() == 4 {
-        if jokers == 1 {
-            // two pair
-            return 3;
-        }
-
-        if jokers == 2 {
+        if jokers > 0 {
             // three of a kind
             return 4;
         }
@@ -294,8 +302,6 @@ fn get_card_rank_v2(cards: &HashMap<char, u32>, jokers: u32) -> u32 {
 
     return 0;
 }
-
-
 
 #[cfg(test)]
 mod tests {
@@ -328,51 +334,95 @@ mod tests {
     #[test]
     fn test_get_card_rank_v2() {
         let cards1 = count_chars("AAAAA");
-        assert_eq!(get_card_rank_v2(&cards1, 0), 7);
+        assert_eq!(get_card_rank_v2(&cards1), 7);
 
         let cards2 = count_chars("AA8AA");
-        assert_eq!(get_card_rank_v2(&cards2, 0), 6);
+        assert_eq!(get_card_rank_v2(&cards2), 6);
 
         let cards3 = count_chars("23332");
-        assert_eq!(get_card_rank_v2(&cards3, 0), 5);
+        assert_eq!(get_card_rank_v2(&cards3), 5);
 
         let cards4 = count_chars("TTT98");
-        assert_eq!(get_card_rank_v2(&cards4, 0), 4);
+        assert_eq!(get_card_rank_v2(&cards4), 4);
 
         let cards5 = count_chars("23432");
-        assert_eq!(get_card_rank_v2(&cards5, 0), 3);
+        assert_eq!(get_card_rank_v2(&cards5), 3);
 
         let cards6 = count_chars("A23A4");
-        assert_eq!(get_card_rank_v2(&cards6, 0), 2);
+        assert_eq!(get_card_rank_v2(&cards6), 2);
 
         let cards7 = count_chars("23456");
-        assert_eq!(get_card_rank_v2(&cards7, 0), 1);
+        assert_eq!(get_card_rank_v2(&cards7), 1);
 
         let cards8 = count_chars("222JJ");
-        assert_eq!(get_card_rank_v2(&cards8, 1), 7);
+        assert_eq!(get_card_rank_v2(&cards8), 7);
 
         let cards9 = count_chars("2222J");
-        assert_eq!(get_card_rank_v2(&cards9, 1), 7);
+        assert_eq!(get_card_rank_v2(&cards9), 7);
 
         let cards10 = count_chars("2223J");
-        assert_eq!(get_card_rank_v2(&cards10, 0), 6);
+        assert_eq!(get_card_rank_v2(&cards10), 6);
 
         let cards11 = count_chars("22JJ4");
-        assert_eq!(get_card_rank_v2(&cards11, 0), 6);
+        assert_eq!(get_card_rank_v2(&cards11), 6);
 
         let cards12 = count_chars("J3455");
-        assert_eq!(get_card_rank_v2(&cards12, 1), 3);
+        assert_eq!(get_card_rank_v2(&cards12), 4);
 
         let cards13 = count_chars("234JJ");
-        assert_eq!(get_card_rank_v2(&cards13, 0), 4);
+        assert_eq!(get_card_rank_v2(&cards13), 4);
 
         let cards14 = count_chars("23455");
-        assert_eq!(get_card_rank_v2(&cards14, 1), 3);
+        assert_eq!(get_card_rank_v2(&cards14), 2);
 
         let cards15 = count_chars("23456");
-        assert_eq!(get_card_rank_v2(&cards15, 1), 2);
+        assert_eq!(get_card_rank_v2(&cards15), 1);
 
         let cards16 = count_chars("2345J");
-        assert_eq!(get_card_rank_v2(&cards16, 1), 2);
+        assert_eq!(get_card_rank_v2(&cards16), 2);
+
+        // 222JJ -> 7 Five of a kind
+        let cards17 = count_chars("222JJ");
+        assert_eq!(get_card_rank_v2(&cards17), 7);
+
+        // 2222J -> 7 Five of a kind
+        let cards18 = count_chars("2222J");
+        assert_eq!(get_card_rank_v2(&cards18), 7);
+
+        // 2223J -> 6 four of kind
+        let cards19 = count_chars("2223J");
+        assert_eq!(get_card_rank_v2(&cards19), 6);
+
+        // 22JJ4 -> 6 four of kind
+        let cards20 = count_chars("22JJ4");
+        assert_eq!(get_card_rank_v2(&cards20), 6);
+
+        // J3455 -> 3 two pair
+        let cards21 = count_chars("J3455");
+        assert_eq!(get_card_rank_v2(&cards21), 4);
+
+        // 234JJ -> 4 three of a kind
+        let cards22 = count_chars("234JJ");
+        assert_eq!(get_card_rank_v2(&cards22), 4);
+
+        // 2345J -> 2 one pair
+        let cards23 = count_chars("2345J");
+        assert_eq!(get_card_rank_v2(&cards23), 2);
+
+        // KTJJT -> 6 four of a kind
+        let cards24 = count_chars("KTJJT");
+        assert_eq!(get_card_rank_v2(&cards24), 6);
+
+        // QQQJA -> 6 four of a kind
+        let cards25 = count_chars("QQQJA");
+        assert_eq!(get_card_rank_v2(&cards25), 6);
+
+        // T55J5 -> 6 four of a kind
+        let cards26 = count_chars("T55J5");
+        assert_eq!(get_card_rank_v2(&cards26), 6);
+
+        // J5745 -> 4 Three of a kind
+        let cards27 = count_chars("J5745");
+        assert_eq!(get_card_rank_v2(&cards27), 4);
     }
 }
