c0b3c106d244de957058cec4622d50318c24585e
Mateusz Swirski <mateusz@payzilch.com>
2023-12-08 23:08:08 +0000

    working day 8-1

--- advent_of_code_2023_8/src/main.rs
+++ advent_of_code_2023_8/src/main.rs
@@ -5,9 +5,9 @@ use std::path::Path;
 
 #[derive(Debug)]
 struct CamelMapEntry {
-    left: &'static str,
-    right: &'static str,
-    name: &'static str,
+    left: String,
+    right: String,
+    name: String,
 }
 
 fn main() {
@@ -17,35 +17,37 @@ fn main() {
     let path: &Path = Path::new("input.txt");
     let file: File = File::open(&path).unwrap();
     let reader: io::BufReader<File> = io::BufReader::new(file);
-    let mut current_positions_v2: Vec<&str> = Vec::new();
+    let mut current_positions_v2: Vec<String> = Vec::new();
 
     let mut directions: String = String::new();
 
     for (index, line) in reader.lines().enumerate() {
         match line {
             Ok(line) => {
-                if line.is_empty() {
+                if line.len() == 0 {
                     continue;
                 }
 
                 if index == 0 {
-                    directions = line.clone();
+                    directions = Some(line).unwrap();
                     println!("First line: {}", directions);
                 } else if index > 1 {
                     let parts: Vec<&str> = line.splitn(2, '=').collect();
-                    let name: &str = parts[0].trim();
+                    let name: String = parts[0].trim().to_string();
                     let parts2: Vec<&str> = parts[1]
-                        .trim_matches(|c| c == '(' || c == ')')
+                        .trim()
+                        .trim_matches('(')
+                        .trim_matches(')')
                         .splitn(2, ',')
                         .collect();
-                    let left: &str = parts2[0].trim();
-                    let right: &str = parts2[1].trim();
+                    let left: String = parts2[0].trim().to_string();
+                    let right: String = parts2[1].trim().to_string();
                     let map_entry: CamelMapEntry = CamelMapEntry {
-                        left,
-                        right,
-                        name,
+                        left: left.clone(),
+                        right: right.clone(),
+                        name: name.clone(),
                     };
-                    camel_map.insert(name.to_owned(), map_entry);
+                    camel_map.insert(name.clone(), map_entry);
 
                     if name.ends_with('A') {
                         current_positions_v2.push(name);
@@ -60,14 +62,14 @@ fn main() {
     // println!("CamelMapEntry: {:?}", camel_map);
 
     // while current position != ZZZ
-    let mut current_position: String = "AAA".to_owned();
+    let mut current_position: String = "AAA".to_string();
     let mut steps: i32 = 0;
-    while current_position != "ZZZ" {
+    loop {
         for c in directions.chars() {
             if c == 'L' {
-                current_position = camel_map.get(&current_position).unwrap().left.to_owned();
+                current_position = camel_map.get(&current_position).unwrap().left.clone();
             } else if c == 'R' {
-                current_position = camel_map.get(&current_position).unwrap().right.to_owned();
+                current_position = camel_map.get(&current_position).unwrap().right.clone();
             }
 
             steps += 1;
@@ -76,13 +78,16 @@ fn main() {
                 break;
             }
         }
+        if current_position == "ZZZ" {
+            break;
+        }
     }
 
     println!("Steps: {}", steps);
 
     let mut steps_v2: u64 = 0;
     let mut break_out_v2 = false;
-    while !break_out_v2 {
+    loop {
         for c in directions.chars() {
             travel_v2(c, &camel_map, &mut current_positions_v2);
 
@@ -93,6 +98,9 @@ fn main() {
                 break;
             }
         }
+        if break_out_v2 {
+            break;
+        }
     }
 
     println!("Steps v2: {}", steps_v2);
@@ -101,19 +109,25 @@ fn main() {
 fn travel_v2(
     direction: char,
     camel_map: &HashMap<String, CamelMapEntry>,
-    current_positions: &mut Vec<&str>,
+    current_positions: &mut Vec<String>,
 ) {
     if direction == 'L' {
-        for position in current_positions {
-            *position = camel_map.get(position).unwrap().left;
+        for i in 0..current_positions.len() {
+            current_positions[i] = camel_map.get(&current_positions[i]).unwrap().left.clone();
         }
     } else if direction == 'R' {
-        for position in current_positions {
-            *position = camel_map.get(position).unwrap().right;
+        for i in 0..current_positions.len() {
+            current_positions[i] = camel_map.get(&current_positions[i]).unwrap().right.clone();
         }
     }
 }
 
-fn are_we_there_yet_v2(current_positions: &[&str]) -> bool {
-    current_positions.iter().all(|position| position.ends_with('Z'))
+fn are_we_there_yet_v2(current_positions: &Vec<String>) -> bool {
+    for position in current_positions {
+        if !position.ends_with('Z') {
+            return false;
+        }
+    }
+
+    return true;
 }
