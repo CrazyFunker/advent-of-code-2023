77cc67e3fd030b0e22d05cf5bcd125209727745f
Mateusz Swirski <mateusz@payzilch.com>
2023-12-16 13:05:46 +0100

    working day 11-2

--- advent_of_code_2023_11/src/main.rs
+++ advent_of_code_2023_11/src/main.rs
@@ -6,12 +6,14 @@ struct Galaxy {
     x: u32,
     y: u32,
 }
-
+// 9648398
 fn main() {
     let mut cosmos: Vec<Vec<char>> = Vec::new();
-    let mut cosmos_rows_to_duplicate: Vec<u32> = Vec::new();
+    let mut cosmos_rows_to_duplicate: Vec<usize> = Vec::new();
     let mut galaxies_in_columns: Vec<u32> = Vec::new();
     let mut galaxies: Vec<Galaxy> = Vec::new();
+    let expansion_amount = 1_000_000;
+    // let expansion_amount = 1;
 
     // read in cosmos
     let path: &Path = Path::new("input.txt");
@@ -40,7 +42,7 @@ fn main() {
                     cosmos_line.push(c);
                 }
                 if !cosmos_line_with_galaxy {
-                    cosmos_rows_to_duplicate.push(index as u32);
+                    cosmos_rows_to_duplicate.push(index);
                 }
 
                 cosmos.push(cosmos_line);
@@ -51,39 +53,38 @@ fn main() {
         }
     }
 
-    // prepare expanded cosmos
-    // expand horizontally
-    for cosmos_line in &mut cosmos {
-        for i in (0..galaxies_in_columns.len()).rev() {
-            if galaxies_in_columns[i] == 0 {
-                cosmos_line.insert(i, '.');
-            }
-        }
-    }
-
-    // expand vertically
-    for i in (0..cosmos_rows_to_duplicate.len()).rev() {
-        let cosmos_line: Vec<char> = cosmos[cosmos_rows_to_duplicate[i] as usize].clone();
-        cosmos.insert(cosmos_rows_to_duplicate[i] as usize, cosmos_line);
-    }
-
     // read in every galaxy
+    let mut empty_rows_encountered: u32 = 0;
     for (index, cosmos_line) in cosmos.iter().enumerate() {
+        if cosmos_rows_to_duplicate.contains(&index) {
+            empty_rows_encountered += 1;
+        }
+
+        let adjusted_y: u32 = (empty_rows_encountered * (expansion_amount - 1)) + index as u32;
+
+        let mut empty_cols_encountered: u32 = 0;
         for (c_index, c) in cosmos_line.iter().enumerate() {
+            if galaxies_in_columns[c_index] == 0 {
+                empty_cols_encountered += 1;
+            }
+
+            let adjusted_x: u32 =
+                (empty_cols_encountered * (expansion_amount - 1)) + c_index as u32;
+
             if *c == '#' {
                 galaxies.push(Galaxy {
-                    x: c_index as u32,
-                    y: index as u32,
+                    x: adjusted_x as u32,
+                    y: adjusted_y as u32,
                 });
             }
         }
     }
 
-    let mut path_sum: u32 = 0;
+    let mut path_sum: u64 = 0;
     // for each pair of galaxies, get the shortest path
     for i in 0..galaxies.len() {
         for j in i + 1..galaxies.len() {
-            let path: u32 = get_shortest_path(&galaxies[i], &galaxies[j]);
+            let path: u64 = get_shortest_path(&galaxies[i], &galaxies[j]);
             path_sum += path;
         }
     }
@@ -91,9 +92,9 @@ fn main() {
     println!("path_sum: {}", path_sum);
 }
 
-fn get_shortest_path(galaxy1: &Galaxy, galaxy2: &Galaxy) -> u32 {
-    let mut x: u32;
-    let mut y: u32;
+fn get_shortest_path(galaxy1: &Galaxy, galaxy2: &Galaxy) -> u64 {
+    let x: u32;
+    let y: u32;
     if galaxy1.x > galaxy2.x {
         x = galaxy1.x - galaxy2.x;
     } else {
@@ -106,5 +107,5 @@ fn get_shortest_path(galaxy1: &Galaxy, galaxy2: &Galaxy) -> u32 {
         y = galaxy2.y - galaxy1.y;
     }
 
-    return x + y;
+    return (x + y) as u64;
 }
